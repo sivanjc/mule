eb99bdbd4ce7d3da3af166765cc38ad64f1d890e
Martín Buchwald <martin.buchwald@mulesoft.com>
2022-10-06 14:12:17 -0300

    writing validation

--- runtime-extension-model/src/main/java/org/mule/runtime/config/internal/validation/CoreValidationsProvider.java
+++ runtime-extension-model/src/main/java/org/mule/runtime/config/internal/validation/CoreValidationsProvider.java
@@ -85,6 +85,7 @@ public class CoreValidationsProvider implements ValidationsProvider {
                                                                  () -> getExpressionSyntacticValidationErrorLevel(featureFlaggingService),
                                                                  Severity.ERROR));
       validations.add(new ExpressionParametersSyntacticallyValid(expressionLanguage, () -> WARN, WARNING));
+      validations.add(new OperationDoesNotHaveInsecureExpression(expressionLanguage));
     }
 
     return validations;
