f9f01dc10e7814bc1006bf039c8cb98e0df2f3b7
Axel Sanguinetti <asanguinetti@mulesoft.com>
2023-04-28 15:52:11 -0300

    Filtering.

--- modules/mule-module-orchestrator/src/main/java/org/mule/runtime/module/orchestrator/internal/OrchestratorAstPostProcessor.java
+++ modules/mule-module-orchestrator/src/main/java/org/mule/runtime/module/orchestrator/internal/OrchestratorAstPostProcessor.java
@@ -6,25 +6,65 @@
  */
 package org.mule.runtime.module.orchestrator.internal;
 
+import static org.mule.runtime.ast.api.util.MuleArtifactAstCopyUtils.copyRecursively;
+import static org.mule.runtime.module.artifact.activation.api.extension.discovery.ExtensionModelDiscoverer.discoverRuntimeExtensionModels;
+
+import static java.util.stream.Collectors.toSet;
+
 import org.mule.runtime.api.meta.model.ExtensionModel;
+import org.mule.runtime.api.meta.model.config.ConfigurationModel;
+import org.mule.runtime.api.meta.model.operation.OperationModel;
 import org.mule.runtime.ast.api.ArtifactAst;
 import org.mule.runtime.ast.api.ComponentAst;
 import org.mule.runtime.config.internal.model.ApplicationModelAstPostProcessor;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class OrchestratorAstPostProcessor implements ApplicationModelAstPostProcessor {
 
+  // We don't want to transform any component associated with the built-in extension models
+  private static final Set<String> DO_NOT_REPLACE_NAMESPACES = discoverRuntimeExtensionModels().stream()
+      .map(em -> em.getXmlDslModel().getNamespace())
+      .collect(toSet());
+
   @Override
   public ArtifactAst postProcessAst(ArtifactAst ast, Set<ExtensionModel> extensionModels) {
-    // TODO: implement
-    return null;
+    return copyRecursively(ast, this::transformComponentAst);
   }
 
   @Override
   public Set<ComponentAst> resolveRootComponents(Collection<ComponentAst> rootComponents, Set<ExtensionModel> extensionModels) {
+    // TODO: check if a non-identity implementation is actually needed
+    return new HashSet<>(rootComponents);
+  }
+
+  private ComponentAst transformComponentAst(ComponentAst componentAst) {
+    ExtensionModel extensionModel = componentAst.getExtensionModel();
+
+    if (DO_NOT_REPLACE_NAMESPACES.contains(extensionModel.getXmlDslModel().getNamespace())) {
+      return componentAst;
+    }
+
+    Optional<ConfigurationModel> configurationModel = componentAst.getModel(ConfigurationModel.class);
+    Optional<OperationModel> operationModel = componentAst.getModel(OperationModel.class);
+    if (configurationModel.isPresent()) {
+      transformConfiguration(componentAst);
+    } else if (operationModel.isPresent()) {
+      transformOperation(componentAst);
+    }
+    return componentAst;
+  }
+
+  private ComponentAst transformConfiguration(ComponentAst componentAst) {
     // TODO: implement
-    return null;
+    return componentAst;
+  }
+
+  private ComponentAst transformOperation(ComponentAst componentAst) {
+    // TODO: implement
+    return componentAst;
   }
 }
